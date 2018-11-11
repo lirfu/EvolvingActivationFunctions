53062fa5d0e9a6188aa03d131216b8086aa57f35
lirfu <jurajfulir@gmail.com>
2018-11-11 09:57:23 +0100

    Add Randomizer for randomizing the dataset

---
+++ src/hr/fer/zemris/data/modifiers/Randomizer.java
@@ -0,0 +1,52 @@
+package hr.fer.zemris.data.modifiers;
+
+import com.sun.istack.internal.NotNull;
+import hr.fer.zemris.utils.Pair;
+
+import java.util.Random;
+
+/**
+ * Randomizes the order of given dataset array elements.
+ * Randomizes by doing <code>n</code> swaps of randomly selected elements.
+ */
+public class Randomizer implements IModifier {
+    private int n_;
+    private Random r_;
+
+    /**
+     * Randomizes the order of given dataset array elements.
+     * Randomizes by doing <code>n</code> swaps of randomly selected elements.
+     * Creates a new random generator.
+     *
+     * @param n Number of random swaps performed on the dataset.
+     */
+    public Randomizer(int n) {
+        this(n, new Random());
+    }
+
+    /**
+     * Randomizes the order of given dataset array elements.
+     * Randomizes by doing <code>n</code> swaps of randomly selected elements.
+     * Uses the given random generator.
+     *
+     * @param n      Number of random swaps performed on the dataset.
+     * @param random Random generator used for swapping.
+     */
+    public Randomizer(int n, @NotNull Random random) {
+        if (n < 0) throw new IllegalArgumentException("Swaps number must not be negative.");
+        n_ = n;
+        r_ = random;
+    }
+
+    @Override
+    public void apply(Pair<float[], Float>[] data) {
+        int len = data.length;
+        for (int i = 0; i < n_; i++) {
+            int a = r_.nextInt(len);
+            int b = r_.nextInt(len);
+            Pair<float[], Float> t = data[a];
+            data[a] = data[b];
+            data[b] = t;
+        }
+    }
+}
