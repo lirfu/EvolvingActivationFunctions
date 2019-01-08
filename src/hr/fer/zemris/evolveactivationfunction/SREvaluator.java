4627a39b9ea22c0ae2ee9811c66653b5837db74b
lirfu <jurajfulir@gmail.com>
2019-01-08 01:23:44 +0100

    Add SREvaluator a flag to memorize already visited trees and their fitnesses

--- src/hr/fer/zemris/evolveactivationfunction/SREvaluator.java
+++ src/hr/fer/zemris/evolveactivationfunction/SREvaluator.java
@@ -11,11 +11,16 @@ import org.nd4j.linalg.activations.IActivation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class SREvaluator extends AEvaluator<DerivableSymbolicTree> {
     private TrainProcedure tmpl_procedure_;
     private int[] architecture_;
     private ILogger log_;
+
+    private boolean use_memory_;
+    private HashMap<String, Double> memory = new HashMap<>();
 
     public SREvaluator(TrainProcedure template_procedure, int[] architecture, ILogger log) {
         tmpl_procedure_ = template_procedure;
@@ -23,17 +28,30 @@ public class SREvaluator extends AEvaluator<DerivableSymbolicTree> {
         log_ = log;
     }
 
+    public SREvaluator(TrainProcedure template_procedure, int[] architecture, ILogger log, boolean use_memory) {
+        this(template_procedure, architecture, log);
+        use_memory_ = use_memory;
+    }
+
     @Override
     public double performEvaluate(DerivableSymbolicTree g) {
+        String s = g.serialize();
+        Double fitness;
+        if (use_memory_ && (fitness = memory.get(s)) != null) {
+            return fitness;
+        }
+
         IActivation[] activations = new IActivation[architecture_.length];
         for (int i = 0; i < activations.length; i++)
             activations[i] = new CustomFunction(g.copy());
 
-        System.out.println("Evaluating: " + g.serialize());
+        System.out.println("Evaluating: " + s);
         CommonModel model = tmpl_procedure_.createModel(architecture_, activations);
 
         tmpl_procedure_.train(model, log_, null);
         Pair<ModelReport, INDArray> res = tmpl_procedure_.test(model);
+
+        // TODO Store ot write.
 
         return res.getKey().f1();
     }
