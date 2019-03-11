ebff30fb8c6e34d40907cebecf37e53e86957faa
lirfu <jurajfulir@gmail.com>
2019-03-11 18:20:40 +0100

    WIP Learnable node gradient pass

--- src/hr/fer/zemris/evolveactivationfunction/activationfunction/DerivableNode.java
+++ src/hr/fer/zemris/evolveactivationfunction/activationfunction/DerivableNode.java
@@ -6,25 +6,38 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 
 public abstract class DerivableNode extends TreeNode<INDArray, INDArray> {
     private IDerivable derivable_;
+    private ILDerivable lderivable_;
 
     public interface IDerivable {
         public INDArray derivate(INDArray input, TreeNode<INDArray, INDArray> node);
     }
 
+    public interface ILDerivable {
+        public INDArray derivate(INDArray input, INDArray epsilon, TreeNode<INDArray, INDArray> node);
+    }
+
     protected DerivableNode(String name, int children_num) {
         super(name, children_num);
         derivable_ = getDerivable();
+        lderivable_ = getLDerivable();
     }
 
     protected DerivableNode(String name, int children_num, Object extra) {
         super(name, children_num, extra);
         derivable_ = getDerivable();
+        lderivable_ = getLDerivable();
     }
 
     public abstract IDerivable getDerivable();
 
+    public abstract ILDerivable getLDerivable();
+
     public INDArray derivate(INDArray input) {
         return derivable_.derivate(input, this);
+    }
+
+    public INDArray lderivate(INDArray input, INDArray epsilon) {
+        return lderivable_.derivate(input, epsilon, this);
     }
 
     @Override
@@ -34,6 +47,10 @@ public abstract class DerivableNode extends TreeNode<INDArray, INDArray> {
         IDerivable der = derivable_;
         derivable_ = ((DerivableNode) n).derivable_;
         ((DerivableNode) n).derivable_ = der;
+
+        ILDerivable lder = lderivable_;
+        lderivable_ = ((DerivableNode) n).lderivable_;
+        ((DerivableNode) n).lderivable_ = lder;
     }
 
     @Override
@@ -43,6 +60,10 @@ public abstract class DerivableNode extends TreeNode<INDArray, INDArray> {
         IDerivable der = derivable_;
         derivable_ = ((DerivableNode) n).derivable_;
         ((DerivableNode) n).derivable_ = der;
+
+        ILDerivable lder = lderivable_;
+        lderivable_ = ((DerivableNode) n).lderivable_;
+        ((DerivableNode) n).lderivable_ = lder;
     }
 
     @Override
--- src/hr/fer/zemris/evolveactivationfunction/nodes/LearnableNode.java
+++ src/hr/fer/zemris/evolveactivationfunction/nodes/LearnableNode.java
@@ -4,10 +4,8 @@ import hr.fer.zemris.evolveactivationfunction.activationfunction.DerivableNode;
 import hr.fer.zemris.genetics.symboregression.IExecutable;
 import hr.fer.zemris.genetics.symboregression.IInstantiable;
 import hr.fer.zemris.genetics.symboregression.TreeNode;
-import org.deeplearning4j.nn.layers.feedforward.PReLU;
 import org.nd4j.linalg.activations.impl.ActivationPReLU;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.api.ops.DynamicCustomOp;
 import org.nd4j.linalg.factory.Nd4j;
 
 /**
@@ -15,29 +13,40 @@ import org.nd4j.linalg.factory.Nd4j;
  */
 public class LearnableNode extends DerivableNode {
     public static final String NAME = "l";
+
+    private INDArray const_input = Nd4j.scalar(-1.f);
     private INDArray alpha;
+    private ActivationPReLU prelu;
 
     public LearnableNode() {
-        this(0.);
+        this(1.);
     }
 
     public LearnableNode(double init_value) {
-        super(NAME, 0, init_value);
+        super(NAME, 0);
+        alpha = Nd4j.scalar(1.);
+        super.extra_ = alpha;  // Set the parameter as extra.
+        prelu = new ActivationPReLU(alpha);
+    }
+
+    public INDArray getAlpha() {
+        return alpha;
     }
 
     @Override
     protected IExecutable<INDArray, INDArray> getExecutable() {
-        return (input, node) -> {//TODO
-            DynamicCustomOp.DynamicCustomOpsBuilder prelu = DynamicCustomOp.builder("prelu").addOutputs(input).addInputs(input, alpha);
-            Nd4j.getExecutioner().exec(prelu.build());
-            return input;
-        };
+        return (input, node) -> prelu.getActivation(const_input, false);
     }
 
     @Override
     public IDerivable getDerivable() {
-        return (input, node) -> Nd4j.zerosLike(input);//TODO
+        return (input, node) -> Nd4j.scalar(0.); // Not dependent on input.
 
+    }
+
+    @Override
+    public ILDerivable getLDerivable() {
+        return (input, epsilon, node) -> prelu.backprop(const_input, epsilon).getFirst(); // TODO Does this update alpha? Check in org.deeplearning4j.nn.layers.feedforward.PReLU
     }
 
     @Override
