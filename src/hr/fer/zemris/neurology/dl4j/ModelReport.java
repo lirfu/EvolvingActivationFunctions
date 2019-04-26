1fb881eb507df25a5e96aa6797d38e9872fc60ba
lirfu <jurajfulir@gmail.com>
2019-04-27 00:30:56 +0200

    Report micro values as well

--- src/hr/fer/zemris/neurology/dl4j/ModelReport.java
+++ src/hr/fer/zemris/neurology/dl4j/ModelReport.java
@@ -2,6 +2,7 @@ package hr.fer.zemris.neurology.dl4j;
 
 import hr.fer.zemris.utils.ISerializable;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.evaluation.EvaluationAveraging;
 import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.evaluation.classification.ROCMultiClass;
 
@@ -10,7 +11,9 @@ import org.nd4j.evaluation.classification.ROCMultiClass;
  */
 public class ModelReport implements IReport, ISerializable {
     private String name_, confusion_matrix_;
-    private double score_, accuracy_, precision_, recall_, f1_, auc_, aucpr_;
+    private double score_, accuracy_, auc_, aucpr_;
+    private double precision_macro_, recall_macro_, f1_macro_;
+    private double precision_micro_, recall_micro_, f1_micro_;
 
     public ModelReport() {
     }
@@ -20,9 +23,15 @@ public class ModelReport implements IReport, ISerializable {
         name_ = params.name();
         score_ = network.score();
         accuracy_ = eval.accuracy();
-        precision_ = eval.precision();
-        recall_ = eval.recall();
-        f1_ = eval.f1();
+
+        precision_macro_ = eval.precision(EvaluationAveraging.Macro);
+        recall_macro_ = eval.recall(EvaluationAveraging.Macro);
+        f1_macro_ = eval.f1(EvaluationAveraging.Macro);
+
+        precision_micro_ = eval.precision(EvaluationAveraging.Micro);
+        recall_micro_ = eval.recall(EvaluationAveraging.Micro);
+        f1_micro_ = eval.f1(EvaluationAveraging.Micro);
+
         auc_ = roc.calculateAverageAUC();
         aucpr_ = roc.calculateAverageAUCPR();
         confusion_matrix_ = eval.confusionMatrix();
@@ -45,15 +54,27 @@ public class ModelReport implements IReport, ISerializable {
     }
 
     public double precision() {
-        return precision_;
+        return precision_macro_;
     }
 
     public double recall() {
-        return recall_;
+        return recall_macro_;
     }
 
     public double f1() {
-        return f1_;
+        return f1_macro_;
+    }
+
+    public double precision_micro() {
+        return precision_micro_;
+    }
+
+    public double recall_micro() {
+        return recall_micro_;
+    }
+
+    public double f1_micro() {
+        return f1_micro_;
     }
 
     public double auc() {
@@ -88,13 +109,22 @@ public class ModelReport implements IReport, ISerializable {
                     accuracy_ = Double.parseDouble(parts[1]);
                     break;
                 case "precision":
-                    precision_ = Double.parseDouble(parts[1]);
+                    precision_macro_ = Double.parseDouble(parts[1]);
                     break;
                 case "recall":
-                    recall_ = Double.parseDouble(parts[1]);
+                    recall_macro_ = Double.parseDouble(parts[1]);
                     break;
                 case "f1":
-                    f1_ = Double.parseDouble(parts[1]);
+                    f1_macro_ = Double.parseDouble(parts[1]);
+                    break;
+                case "precision_micro":
+                    precision_micro_ = Double.parseDouble(parts[1]);
+                    break;
+                case "recall_micro":
+                    recall_micro_ = Double.parseDouble(parts[1]);
+                    break;
+                case "f1_micro":
+                    f1_micro_ = Double.parseDouble(parts[1]);
                     break;
                 case "auc":
                     auc_ = Double.parseDouble(parts[1]);
@@ -117,9 +147,12 @@ public class ModelReport implements IReport, ISerializable {
                 .append("name").append('\t').append(name_).append('\n')
                 .append("score").append('\t').append(score_).append('\n')
                 .append("accuracy").append('\t').append(accuracy_).append('\n')
-                .append("precision").append('\t').append(precision_).append('\n')
-                .append("recall").append('\t').append(recall_).append('\n')
-                .append("f1").append('\t').append(f1_).append('\n')
+                .append("precision").append('\t').append(precision_macro_).append('\n')
+                .append("recall").append('\t').append(recall_macro_).append('\n')
+                .append("f1").append('\t').append(f1_macro_).append('\n')
+                .append("precision_micro").append('\t').append(precision_micro_).append('\n')
+                .append("recall_micro").append('\t').append(recall_micro_).append('\n')
+                .append("f1_micro").append('\t').append(f1_micro_).append('\n')
                 .append("auc").append('\t').append(auc_).append('\n')
                 .append("auc_pr").append('\t').append(aucpr_).append('\n')
                 .append("confusion_matrix").append('\n').append(confusion_matrix_)
