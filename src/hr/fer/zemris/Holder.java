08aa9b28c5ce4cca055c90566d4d2894fe9805d0
lirfu <jurajfulir@gmail.com>
2019-05-28 15:27:32 +0200

    Add Holder, the utility class for syncing

---
+++ src/hr/fer/zemris/Holder.java
@@ -0,0 +1,27 @@
+package hr.fer.zemris;
+
+/**
+ * Utility class for holding an instance while defined as final. Usages include holding an instance in multithreaded setup.
+ */
+public class Holder<T> {
+    private T item_;
+
+    public Holder() {
+    }
+
+    public Holder(T item) {
+        item_ = item;
+    }
+
+    public synchronized void set(T item) {
+        item_ = item;
+    }
+
+    public T get() {
+        return item_;
+    }
+
+    public boolean isDefined() {
+        return item_ != null;
+    }
+}
